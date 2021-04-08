ffe7a95d459308ef8872e81d9be32cffe7edfca1
Dmitrii Sosedov <dsosedov@vmware.com>
2021-04-07 21:01:43 -0400

    Init

---
+++ src/main/java/com/dsosedov/filepathapi/FilepathApiApplication.java
@@ -0,0 +1,13 @@
+package com.dsosedov.filepathapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilepathApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilepathApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsosedov/filepathapi/FilepathApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsosedov.filepathapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilepathApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
